93dffb58a03f7ed5fee4fde9015a166d090321f8
Vik.v.zayats@gmail.com <vik.v.zayats@gmail.com>
2020-07-28 16:29:49 +0300

    From Work

--- src/by/zvv/javaonline/part04/aggregation/task05/entity/Request.java
+++ src/by/zvv/javaonline/part04/aggregation/task05/entity/Request.java
@@ -1,5 +1,74 @@
 package by.zvv.javaonline.part04.aggregation.task05.entity;
 
-public class Request {
+import by.zvv.javaonline.part04.aggregation.task05.enums.Food;
+import by.zvv.javaonline.part04.aggregation.task05.enums.Transport;
+import by.zvv.javaonline.part04.aggregation.task05.enums.Type;
 
+public class Request {
+	private String place;
+	private Food food;
+	private Transport trans;
+	private Type type;
+	private int days;
+	private int price;
+	
+	public Request() {
+		
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public Food getFood() {
+		return food;
+	}
+
+	public Transport getTrans() {
+		return trans;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public int getDays() {
+		return days;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public void setFood(Food food) {
+		this.food = food;
+	}
+
+	public void setTrans(Transport trans) {
+		this.trans = trans;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public void setDays(int days) {
+		this.days = days;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Request [place=" + place + ", food=" + food + ", trans=" + trans + ", type=" + type + ", days=" + days
+				+ ", price=" + price + "]";
+	}
+	
+	
 }
